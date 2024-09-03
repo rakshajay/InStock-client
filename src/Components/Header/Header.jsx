879f27ea8c8d039e2857f29003302286e910a709
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-03 15:33:30 -0700

    add html of header, import logo and icons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import "./header.scss";
+import { Link } from "react-router-dom";
+import logoImg from "../Assets/Logo/InStock-Logo_2x.png";
 
 function Header() {
   return (
-    <header>
-      <div></div>
+    <header className="header">
+      <div className="header__wrapper-logo">
+        <img src={logoImg} alt="" />
+      </div>
+      <div className="header__wrapper-buttons">
+        <Link to="/warehouses" className="header__link">
+          <button className="header__button">Warehouses</button>
+        </Link>
+        <Link to="/inventory" className="header__link">
+          <button className="header__button">Inventory</button>
+        </Link>
+      </div>
     </header>
   );
 }
