77a1398909339c67c0a2d1e63ce38a1ca7b38497
rakshajay <rakshashettyhs@gmail.com>
2024-09-04 12:11:44 -0700

    jsx from 1.DeleteWarehouse, 2. DeleteInventory, 3.Footer, 4.AddNewInventory, 5.AddNewWarehouse

--- src/Components/AddNewWarehouse/AddNewWarehouse.jsx
+++ src/Components/AddNewWarehouse/AddNewWarehouse.jsx
@@ -2,8 +2,52 @@ import "./AddNewWarehouse.scss";
 
 function AddNewWarehouse() {
     return (
-      <>
-      </>
+      <form>
+        <div className="section">
+          <h2>Warehouse Details</h2>
+          <label>
+            Warehouse Name
+            <input type="text" name="warehouseName" placeholder="Warehouse Name" />
+          </label>
+          <label>
+            Street Address
+            <input type="text" name="streetAddress" placeholder="Street Address" />
+          </label>
+          <label>
+            City
+            <input type="text" name="city" placeholder="City" />
+          </label>
+          <label>
+            Country
+            <input type="text" name="country" placeholder="Country" />
+          </label>
+        </div>
+
+        <div className="section">
+          <h2>Contact Details</h2>
+          <label>
+            Contact Name
+            <input type="text" name="contactName" placeholder="Contact Name" />
+          </label>
+          <label>
+            Position
+            <input type="text" name="position" placeholder="Position" />
+          </label>
+          <label>
+            Phone Number
+            <input type="text" name="phoneNumber" placeholder="Phone Number" />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" placeholder="Email" />
+          </label>
+        </div>
+
+        <div className="buttons">
+          <button type="button" className="cancel-button">Cancel</button>
+          <button type="submit" className="submit-button">+Add Warehouse</button>
+        </div>
+      </form>
     );
   }
   
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,8 +2,11 @@ import "./Footer.scss";
 
 function Footer() {
     return (
-      <>
-      </>
+      <footer>
+        <h4 >
+        © InStock Inc. All Rights Reserved.
+        </h4>
+      </footer>
     );
   }
   
