59c72d4f8d2f5ac98c22d754e5676841240d5a96
Zanab Shah <zanab@protonmail.com>
2024-09-05 09:53:10 -0700

    Created shared components: List, ListItem, Icon + Warehouse and Inventorry list pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import "./App.scss";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Inventory from "./Pages/Inventory/Inventory";
+import AddNewInventory from "./Components/AddNewInventory/AddNewInventory";
+import InventoryItemDetails from "./Components/InventoryItemDetails/InventoryItemDetails";
+import EditInventoryItem from "./Components/EditInventoryItem/EditInventoryItem";
+import AddNewWarehouse from "./Components/AddNewWarehouse/AddNewWarehouse";
+import EditWarehouse from "./Components/EditWarehouse/EditWarehouse";
+import Warehouses from "./Pages/Warehouses/Warehouses";
+import WarehouseDetails from "./Components/WarehouseDetails/WarehouseDetails";
 
 // import xyz component from 'component'...
 
@@ -10,37 +18,47 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-        <div id="app__body">
-          <Routes>
-            <Route path="/" element={<WarehouseList />} />
-            <Route
-              path="/warehouse/:warehouseId"
-              element={<WarehouseDetails />}
-            />
-            <Route
-              path="/warehouse/:warehouseId/edit"
-              element={<WarehouseEdit />}
-            />
-            <Route path="/warehouse/add" element={<WarehouseCreate />} />
-            <Route
-              path="/warehouse/:warehouseId/inventory"
-              element={<Inventory />}
-            />
-            <Route
-              path="/warehouse/:warehouseId/inventory/:itemId"
-              element={<ItemDetails />}
-            />
-            <Route
-              path="/warehouse/:warehouseId/inventory/:itemId/edit"
-              element={<ItemEdit />}
-            />
-            <Route
-              path="/warehouse/:warehouseId/inventory/add"
-              element={<ItemAdd />}
-            />
-            <Route path="*" element={<Navigate to={"/"} />} />
-          </Routes>
-        </div>
+          <div id="app__body">
+            <Routes>
+              <Route 
+                path="/"
+                element={<Warehouses />}
+              />
+              <Route 
+                path="/inventory"
+                element={<Inventory />}
+              />
+              <Route 
+                path="/warehouse/:warehouseId" 
+                element={<WarehouseDetails />}
+              />
+              <Route 
+                path="/warehouse/:warehouseId/edit"
+                element={<EditWarehouse />}
+              />
+              <Route 
+                path="/warehouse/add" 
+                element={<AddNewWarehouse />} 
+              />
+              <Route
+                path="/warehouse/:warehouseId/inventory"
+                element={<Inventory />}
+              />
+              <Route
+                path="/warehouse/:warehouseId/inventory/:itemId"
+                element={<InventoryItemDetails />}
+              />
+              <Route
+                path="/warehouse/:warehouseId/inventory/:itemId/edit"
+                element={<EditInventoryItem />}
+              />
+              <Route
+                path="/warehouse/:warehouseId/inventory/add"
+                element={<AddNewInventory />}
+              />
+              <Route path="*" element={<Navigate to={"/"} />} />
+            </Routes>
+          </div>
         <Footer />
       </BrowserRouter>
     </>
